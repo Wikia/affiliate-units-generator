9a928ccf5fc7c594854e9ecb7a385936a2fa20f8
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-11-20 11:03:21 -0800

    Updates

---
+++ precache-manifest.98d4db4b7aec6cc2bdb660763ffbd107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c35809f25adf6365fd3388297d4e944",
+    "url": "/affiliate-units-generator/index.html"
+  },
+  {
+    "revision": "e3ed1274ca69f2ae3e31",
+    "url": "/affiliate-units-generator/static/css/2.99a42bcb.chunk.css"
+  },
+  {
+    "revision": "851d89acca4a4724edb4",
+    "url": "/affiliate-units-generator/static/css/main.f4a25c51.chunk.css"
+  },
+  {
+    "revision": "e3ed1274ca69f2ae3e31",
+    "url": "/affiliate-units-generator/static/js/2.493a27e4.chunk.js"
+  },
+  {
+    "revision": "851d89acca4a4724edb4",
+    "url": "/affiliate-units-generator/static/js/main.9984d5e3.chunk.js"
+  },
+  {
+    "revision": "8217e407ffc3d5c599c5",
+    "url": "/affiliate-units-generator/static/js/runtime-main.0eeb49eb.js"
+  }
+]);
